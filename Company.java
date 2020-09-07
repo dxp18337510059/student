bc9e3f477e459cbc1e4e1a919f3ce57b7c391a73
dxpStudent <dxp18337510059@163.com>
2020-09-07 19:53:59 +0800

    在家开发的其它功能

--- Company.java
+++ Company.java
@@ -2,14 +2,21 @@
 * @Author: Marte
 * @Date:   2020-09-07 19:43:25
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-09-07 19:45:17
+* @Last Modified time: 2020-09-07 19:53:22
 */
 
 public class Company {
     private String name;
+    private String home;
     private int age;
 
     public void eat(){
+        System.out.println("吃饭");
+    }
+    public void eat2(){
+        System.out.println("吃饭");
+    }
+    public void eat3(){
         System.out.println("吃饭");
     }
 }
