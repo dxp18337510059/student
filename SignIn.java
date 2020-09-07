2c3dcb3c377ca4384c9a31e1057ec95eb9d1c2b9
dxpStudent <dxp18337510059@163.com>
2020-09-07 17:02:13 +0800

    在家继续开发签到功能

---
+++ SignIn.java
@@ -0,0 +1,19 @@
+/*
+* @Author: Marte
+* @Date:   2020-09-07 16:45:41
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-09-07 17:00:20
+*/
+
+public class SignIn {
+    private int id;
+    private int age;
+    public void sign(){
+        System.out.println("这是在家开发的签到功能！！");
+    }
+
+    public void sign(int id){
+        System.out.println("这是在家开发的签到功能！！");
+    }
+
+}
