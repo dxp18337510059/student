705fc4143d0342d0fa8ae830fad3ba9758d8e82f
dxpCompany <dxpCompany@163.com>
2020-09-07 19:52:11 +0800

    在公司开发的功能未推送

--- Company.java
+++ Company.java
@@ -2,14 +2,17 @@
 * @Author: Marte
 * @Date:   2020-09-07 19:43:25
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-09-07 19:45:17
+* @Last Modified time: 2020-09-07 19:51:26
 */
 
 public class Company {
     private String name;
-    private int age;
+    private int score;
 
     public void eat(){
         System.out.println("吃饭");
+    }
+    public void sleep(){
+        System.out.println("睡觉");
     }
 }
