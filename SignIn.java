b1a254e6bd6e7f9e894b7ec9b9aa32920d0bb145
dxpCompany <dxpCompany@163.com>
2020-09-07 16:57:12 +0800

    在公司继续开发签到功能

---
+++ SignIn.java
@@ -0,0 +1,15 @@
+/*
+* @Author: Marte
+* @Date:   2020-09-07 16:43:26
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-09-07 16:56:05
+*/
+
+public class SingIn {
+    private int id;
+    private String name;
+
+    public void sign(){
+        System.out.prinln("在公司开发的功能！！");
+    }
+}
