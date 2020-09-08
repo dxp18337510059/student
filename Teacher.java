74de26a1ad60b50f4e0db1b2a796e374b27dc103
dxpStudent <dxp18337510059@163.com>
2020-09-08 10:52:37 +0800

    在家里开发教师查询功能

---
+++ Teacher.java
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2020-09-08 10:50:43
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-09-08 10:51:57
+*/
+
+public class Teacher {
+    private int id;
+    private int age;
+    private void teach(){
+
+    }
+}
