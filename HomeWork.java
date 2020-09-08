6eda1c6367fba97c404ccb6eb297d94dcc3d1299
dxpStudent <dxp18337510059@163.com>
2020-09-08 11:24:10 +0800

    在家继续完善HomeWork

--- HomeWork.java
+++ HomeWork.java
@@ -2,13 +2,17 @@
 * @Author: Marte
 * @Date:   2020-09-07 15:55:07
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-09-08 11:14:29
+* @Last Modified time: 2020-09-08 11:23:39
 */
 
 public class java {
     public static void main(String[] args) {
         // 111111
         // 在公司开发HomeWork功能
+        sout("在家开发HomeWork功能");
+        sout("在家开发HomeWork功能");
+        sout("在家开发HomeWork功能");
+        sout("在家开发HomeWork功能");
         sout("在家开发HomeWork功能");
     }
 }
