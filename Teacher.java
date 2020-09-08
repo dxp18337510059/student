bc3345b2db2ce0809ae961a01fd0b9dbb277d7b9
dxpCompany <dxpCompany@163.com>
2020-09-08 10:49:52 +0800

    在公司开发教师查询

--- Teacher.java
+++ Teacher.java
@@ -1,12 +1,19 @@
 /*
 * @Author: Marte
+<<<<<<< HEAD
 * @Date:   2020-09-08 10:50:43
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-09-08 10:51:57
+* @Last Modified time: 2020-09-08 10:59:33
+=======
+* @Date:   2020-09-08 10:48:47
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-09-08 10:49:20
+>>>>>>> 在公司开发教师查询
 */
 
 public class Teacher {
     private int id;
+    private String name;
     private int age;
     private void teach(){
 
