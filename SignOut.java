b51803b2ce8002291e336bd5f14698afec3f715b
dxpCompany <dxpCompany@163.com>
2020-09-08 09:31:29 +0800

    完成签退功能开发

--- SignOut.java
+++ SignOut.java
@@ -1,22 +1,4 @@
-/*
-* @Author: Marte
-<<<<<<< HEAD
-* @Date:   2020-09-07 16:46:18
-* @Last Modified by:   Marte
-* @Last Modified time: 2020-09-07 19:31:23
-=======
-* @Date:   2020-09-07 16:48:48
-* @Last Modified by:   Marte
-* @Last Modified time: 2020-09-07 19:41:13
->>>>>>> 478ae4ddb488f389cd60d3c3e85a267f7667cadd
-*/
-
-public class java {
-    public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("在公司开发签退功能！");
-=======
-
->>>>>>> 478ae4ddb488f389cd60d3c3e85a267f7667cadd
-    }
+public class SignOut{
+    private String id;
+    private String name;
 }
