3d1aaf5bde1343a7f261f224b50a9c4c50534d18
dxpStudent <dxp18337510059@163.com>
2020-09-08 11:12:02 +0800

    在家开发HomeWork功能

---
+++ HomeWork.java
@@ -0,0 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2020-09-07 15:55:07
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-09-08 11:11:20
+*/
+
+public class java {
+    public static void main(String[] args) {
+        sout("在家开发HomeWork功能");
+    }
+}
