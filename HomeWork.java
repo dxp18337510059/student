5456d7c06670c5861274bf802594580c5dd6ea6e
dxpCompany <dxpCompany@163.com>
2020-09-08 11:22:40 +0800

    在家完善HomeWork功能

--- HomeWork.java
+++ HomeWork.java
@@ -2,7 +2,8 @@
 * @Author: Marte
 * @Date:   2020-09-07 15:55:07
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-09-08 11:23:39
+* @Last Modified time: 2020-09-08 11:26:38
+* @Last Modified time: 2020-09-08 11:21:55
 */
 
 public class java {
@@ -10,6 +11,8 @@ public class java {
         // 111111
         // 在公司开发HomeWork功能
         sout("在家开发HomeWork功能");
+        System.out.println("在公司继续完善HomeWork功能");
+        // 以下是修改冲突
         sout("在家开发HomeWork功能");
         sout("在家开发HomeWork功能");
         sout("在家开发HomeWork功能");
